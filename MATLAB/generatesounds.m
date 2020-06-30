85ef544c26122f139bc0dbad68c714194ef7bd88
Scott97 <scottbarnard5@gmail.com>
2020-06-30 13:40:00 +1000

    Synthesised audio and compared sample rates
    Sounds of individual bubbles are generated from their second order
    differential equations. Each sound is time shifted and added to create
    the sound of many bubbles. I use a matix of source data to generate the
    sounds, the advantage of this is that I can use that source data to
    compare my bubble detection algorithm to the true values of radius and
    time. Synthesising the audio is slow, because it involves solving second
    order differential equations, so to improve performance, audio data is
    memoized (cached), so that when I rerun the script, it can skip these
    calculations.
    
    I compared sample rates of 22050, 44100 and 88200Hz, and I did not see a
    noticable effect on the quality of bubble detection, suggesting that the
    only effect is on nyquist frequency or minimum bubble radius detectable.

---
+++ MATLAB/generatesounds.m
@@ -0,0 +1,32 @@
+function [y,t] = generatesounds(data,fs)
+
+% Constants
+noise_amount = 0.00005;
+
+% Data
+R_0 = data(1,:) ./ 1000; % Convert from mm to m
+t_val = data(2,:) ./ 1000; % Convert from ms to s
+dd_ic = data(3,:);
+
+
+% Make sounds
+t = 0:(1/fs):1.5;
+y = zeros(length(t),1);
+
+
+for i = 1:length(t_val)
+    [~,d] = makesound(R_0(i), dd_ic(i),0:(1/fs):1.5);
+    idx = floor(t_val(i)*fs); % times or divide?
+    shifted = circshift(d,idx,1);
+    y = y + shifted(1:length(y));
+end
+
+% Add some background noise
+noise = randn([length(y),1]) * noise_amount;
+y = y + noise;
+
+% Amplify
+y = y * 10^4 * 0.18;
+
+end
+
---
+++ MATLAB/makesound.m
@@ -0,0 +1,37 @@
+% https://au.mathworks.com/matlabcentral/answers/66301-solve-a-second-order-differential-equation
+
+function [t,d] = makesound(R_0, ddot_0, tspan)
+
+% Constants
+K_p = 1.4; % 1.4
+p_0 = 1000; % 1000 kg/m^3
+P_0 = 101325; % 1 atm or 101,325 Pa
+b = 0.6; % (need to research typical values)
+
+% Calculated Constants
+a = R_0;
+b;
+c = (3 * K_p * P_0) / (p_0 * R_0);
+
+% Initial Conditions
+ic = [0,ddot_0]; % d(0) = 0, d'(0) = ?
+
+% ODE
+% R_0 d''(t) + b d'(t) + 3K_p P_0 / p_0 R_0 d(t) = 0 
+% a d''(t) + b d'(t) + c d(t) = 0 
+
+% let z(1) = d
+% let z(2) = d'
+
+% ∴ dz(1) = d'  = z(2)
+% ∴ dz(2) = d'' = (-b d' -c d) / a = (-b z(2) -c z(1)) / a
+
+dz = @(t,z)[ z(2); (-b * z(2) - c*z(1)) / a];
+
+% Evaluate
+[t,z]=ode45(dz, tspan, ic);
+d = z(:,1);
+
+end
+
+
