038aa408f1b9700058dddb9b3f80315218734076
Scott97 <scottbarnard5@gmail.com>
2020-07-03 11:00:00 +1000

    Improve performance of make sound function
    Rather than solving a second order differential equation numerically,
    which is slow, I solved the equation by hand to get the algebraic
    solution, which I use to generate the sound data.

--- MATLAB/make_sound.m
+++ MATLAB/make_sound.m
@@ -1,36 +1,27 @@
-% https://au.mathworks.com/matlabcentral/answers/66301-solve-a-second-order-differential-equation
+% Solving 2ODE's is slow so this just uses an algebraic expression.
+function [t,d] = make_sound(R_0, ddot_0, t)
 
-function [t,d] = make_sound(R_0, ddot_0, tspan)
+    % Constants
+    K_p = 1.4; % 1.4
+    p_0 = 1000; % 1000 kg/m^3
+    P_0 = 101325; % 1 atm or 101,325 Pa
+    b = 0.6; % (need to research typical values)
 
-% Constants
-K_p = 1.4; % 1.4
-p_0 = 1000; % 1000 kg/m^3
-P_0 = 101325; % 1 atm or 101,325 Pa
-b = 0.6; % (need to research typical values)
+    % Calculated Constants
+    a = R_0;
+    b;
+    c = (3 * K_p * P_0) / (p_0 * R_0);
 
-% Calculated Constants
-a = R_0;
-b;
-c = (3 * K_p * P_0) / (p_0 * R_0);
+    % More Calculated Constants
+    k = b/(2*a);
+    w = sqrt( (c/a) - (k^2) );
+    M = ddot_0/w;
 
-% Initial Conditions
-ic = [0,ddot_0]; % d(0) = 0, d'(0) = ?
 
-% ODE
-% R_0 d''(t) + b d'(t) + 3K_p P_0 / p_0 R_0 d(t) = 0 
-% a d''(t) + b d'(t) + c d(t) = 0 
-
-% let z(1) = d
-% let z(2) = d'
-
-% ∴ dz(1) = d'  = z(2)
-% ∴ dz(2) = d'' = (-b d' -c d) / a = (-b z(2) -c z(1)) / a
-
-dz = @(t,z)[ z(2); (-b * z(2) - c*z(1)) / a];
-
-% Evaluate
-[t,z]=ode45(dz, tspan, ic);
-d = z(:,1);
+    % Evaluate
+    d = M * exp(-k .* t) .* sin(w .* t);
+    d = d';
+    t = t';
 
 end
 
