dd2441807ca7ea80c9cd08e6a6270bd1de7502c6
Scott97 <scottbarnard5@gmail.com>
2020-07-03 11:30:00 +1000

    Remove hardcoded data
    Rather than using hardcoded data for sound synthesis, I use a function
    to generate the data instead. The advantage of this is that I can
    specify the number of bubbles I would like to generate and the duration
    of the sound clip.

---
+++ MATLAB/source_data.m
@@ -0,0 +1,29 @@
+function [radii, timestamps, icdd, xpos, ypos] = source_data(n,duration)
+    % n - number of bubbles
+    % duration - the length in time of data
+
+    % Setup random so its consistent
+    rng(1,'philox');
+
+    % Radii (mm)
+    mu  = 1.500;
+    sig = 0.500;
+    radii = normrnd(mu,sig,1,n);
+
+    % Timestamps (ms)
+    timestamps =  duration.*rand(1,n);
+
+    % Initial conditions (mm/ms)
+    mu  = 5.000;
+    sig = 1.000;
+    icdd = normrnd(mu,sig,1,n);
+
+    % Bubble positions (mm)
+    % Bubbles are placed equally on the surface of the water (2D plane)
+    % Some bubbles outside the measurement area shall be ignored
+    min = -1000;
+    max = +1000;
+    xpos = min + (max-min).*rand(1,n);
+    ypos = min + (max-min).*rand(1,n);
+
+end
