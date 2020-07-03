d13516b3464219d8e3a2617b1d965d61cc5c87ee
Scott97 <scottbarnard5@gmail.com>
2020-07-03 11:45:00 +1000

    Spatial data can be visualised
    The data generation function also specifies bubble location, for in the
    future when I may use multihydrophone or directional hydrophone setups
    where bubble location has an affect on the data captured. This adds a
    function for plotting the spatial data for debugging purposes.

---
+++ MATLAB/plot_spatial_data.m
@@ -0,0 +1,9 @@
+function plot_spatial_data(radii, timestamps, icdd, xpos, ypos)
+    scatter3(xpos, ypos, timestamps, radii .* 25, icdd); % .* 25 is to improve legibility
+    axis tight
+    title("Spatial data - colour: icdd, size: radius")
+    xlabel("X position on surface (mm)")
+    ylabel("Y position on surface (mm)")
+    zlabel("time (ms)")
+    colorbar;
+end
