6967ef4e489f474eb924599e19cf1fd47ccdeb3c
Scott97 <scottbarnard5@gmail.com>
2020-07-23 12:30:00 +1000

    Refactor code
    This adds comments and cleans up code for the fwt and plunging jets
    scripts. The variable scale, is changed to truly represent scale of the
    wavelet, as compared to the inverse of scale previously.

--- MATLAB/fwt.m
+++ MATLAB/fwt.m
@@ -1,13 +1,16 @@
-function [s,f] = fwt(y,wvlt_fn,wvlt_bounds,bands,fs)
-    % Wavelet functions must have a frequency of 1Hz!
+function [s,f] = fwt(y,wvlt_fn,wvlt_bounds,f,fs)
+    % y - signal
+    % wvlt_fn - wavelet function of form @(t), must have a frequency of 1Hz!
+    % wvlt_bounds - an array [min,max], where the wavelet function is equal to zero below min and above max
+    % f - an array of all the frequencies you would like data for
+    % fs - sample rate
 
-    for i = 1:length(bands)
-        scale = bands(i);
-        t = (wvlt_bounds(1)/scale):(1/fs):(wvlt_bounds(2)/scale);
-        wv = wvlt_fn(t*scale);
-        row = conv(y,wv) .* (1/sqrt(1/scale));
+    for i = 1:length(f)
+        scale = 1/f(i);
+        t = (wvlt_bounds(1)*scale):(1/fs):(wvlt_bounds(2)*scale);
+        wv = wvlt_fn(t/scale);
+        row = conv(y,wv) .* (1/sqrt(scale));
         s(i,:) = row(1:length(y));
     end
-    f = bands;
 end
 
--- MATLAB/plunging_jets.m
+++ MATLAB/plunging_jets.m
@@ -1,23 +1,20 @@
+% MATLAB testing my wavelet implementation
+
 % Plunging Jets
 [y,fs] = audioread('../../DATA/PlungingJets/CircularPlungingJet_x9613135_1.wav', [0.0,1.5]*22050+1);
 
-% Analyse
+% Define wavelet
 morlet = @(t) exp(-((2*pi/5) .* t) .^ 2 ./2) .* cos((2*pi).*t);
+
+% Show wavelet
 t = -3:.01:3;
 plot(t,morlet(t))
 
-frequencies = 100:50:9000;
-
-
-[s,f] = fwt(y, morlet, [-3,3], frequencies, 22050);
-
+% CWT
+[s,f] = fwt(y, morlet, [-3 3], 1000:50:9000, 22050);
 [s2,f2] = cwt(y,fs);
-
-size(s)
-size(f)
 
 % Plot
 t = (0:numel(y)-1)/fs;
-plot_utils.scaleogram(s,f,t,[0 1500],[0 9],'my cwt');
-plot_utils.scaleogram(s2,f2,t,[0 1500],[0 9],'matlab cwt');
-
+plot_utils.scaleogram(s,f,t,[0 1500],[1 9],'my cwt');
+plot_utils.scaleogram(s2,f2,t,[0 1500],[1 9],'matlab cwt');
