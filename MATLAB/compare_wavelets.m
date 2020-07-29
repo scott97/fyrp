077f878a53bec7e21be4a2a2aac1417260bd89be
Scott97 <scottbarnard5@gmail.com>
2020-07-29 11:00:00 +1000

    Try out additional wavelets and use find peaks function
    This adds another SOULTI wavelet with a lower zeta value as well as
    another wavelet which is similar to the Morlet wavelet, but with better
    frequency resolution and worse time resolution.
    
    The find peaks function is used on all plots so that I can compare them
    on identification effectiveness.

--- MATLAB/compare_wavelets.m
+++ MATLAB/compare_wavelets.m
@@ -7,7 +7,7 @@ cached_one_hydrophone = memoize(@generate_audio.one_hydrophone);
 [radii, timestamps, icdd, xpos, ypos] = cached_source_data(1000,1500);
 zone = sqrt(xpos.^2 + ypos.^2) < 1000; % I only want to measure bubbles in a 1m radius of the surface
 
-plot_utils.spatial_data(radii, timestamps, icdd, xpos, ypos, zone);
+% plot_utils.spatial_data(radii, timestamps, icdd, xpos, ypos, zone);
 
 source_data = [
 	radii;
@@ -38,9 +38,33 @@ tau = 2*pi;
 
 % Define wavelets
 morlet = @(t) exp(-(tau/5 .* t) .^ 2 ./2) .* cos(tau .* t);
+
+winsin = @(t) exp(-(tau/15 .* t) .^ 2 ./2) .* cos(tau .* t);
+
 zeta = 0.1;
 k = 1-zeta^2;
-soulti = @(t) 1/k .* exp(-zeta/k * tau .* t) .* sin(tau .* t) .* (t>0);
+soulti1 = @(t) 1/k .* exp(-zeta/k * tau .* t) .* sin(tau .* t) .* (t>0);
+
+zeta = 0.02;
+k = 1-zeta^2;
+soulti2 = @(t) 1/k .* exp(-zeta/k * tau .* t) .* sin(tau .* t) .* (t>0);
+
+% Plot wavelets
+% figure
+% t = -3:.01:3;
+% plot(t,morlet(t))
+
+% figure
+% t = -9:.01:9;
+% plot(t,winsin(t))
+
+% figure
+% t = 0:.01:10;
+% plot(t,soulti1(t))
+
+% figure
+% t = 0:.01:50;
+% plot(t,soulti2(t))
 
 % Get data
 [y,t] = cached_one_hydrophone(source_data, fs, loc);
@@ -51,17 +75,31 @@ t = (0:numel(y)-1)/fs;
 
 % Morlet
 [s,f] = bubble_analysis.cwt(y, morlet, [-3 3], 1000:50:9000, fs);
-% peaks = bubble_analysis.find_peaks(s,f,t,0.09);
+peaks = bubble_analysis.find_peaks(s,f,t,0.12);
 plot_utils.scaleogram(s,f,t,[0 1500],[1 9],'my cwt (morlet)');
-% plot_utils.peaks(peaks,'r');
-% plot_utils.peaks(comparison_data,'g');
+plot_utils.peaks(peaks,'r');
+plot_utils.peaks(comparison_data,'g');
 
-% SOULTI
-[s,f] = bubble_analysis.cwt(y, soulti, [0 10], 1000:50:9000, fs);
-% peaks = bubble_analysis.find_peaks(s,f,t,0.09);
-plot_utils.scaleogram(s,f,t,[0 1500],[1 9],'my cwt (SOULTI)');
-% plot_utils.peaks(peaks,'r');
-% plot_utils.peaks(comparison_data,'g');
+% Morlet alt
+[s,f] = bubble_analysis.cwt(y, winsin, [-9 9], 1000:50:9000, fs);
+peaks = bubble_analysis.find_peaks(s,f,t,0.12);
+plot_utils.scaleogram(s,f,t,[0 1500],[1 9],'my cwt (windowed sine)');
+plot_utils.peaks(peaks,'r');
+plot_utils.peaks(comparison_data,'g');
+
+% SOULTI1
+[s,f] = bubble_analysis.cwt(y, soulti1, [0 10], 1000:50:9000, fs);
+peaks = bubble_analysis.find_peaks(s,f,t,0.12);
+plot_utils.scaleogram(s,f,t,[0 1500],[1 9],'my cwt (SOULTI zeta=0.1)');
+plot_utils.peaks(peaks,'r');
+plot_utils.peaks(comparison_data,'g');
+
+% SOULTI2
+[s,f] = bubble_analysis.cwt(y, soulti2, [0 50], 1000:50:9000, fs);
+peaks = bubble_analysis.find_peaks(s,f,t,0.12);
+plot_utils.scaleogram(s,f,t,[0 1500],[1 9],'my cwt (SOULTI zeta=0.02)');
+plot_utils.peaks(peaks,'r');
+plot_utils.peaks(comparison_data,'g');
 
 % MATLAB built-in
 [s,f] = cwt(y,fs);
