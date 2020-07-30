9118211405311344bdaf4b0536a1b7a8ad36e4a7
Scott97 <scottbarnard5@gmail.com>
2020-07-30 12:00:00 +1000

    Improve rust implementation of CWT
    This improves the CWT functionality of my program. The iterator for
    creating ranges of floating point numbers is moved to a separate file
    and unit tests have been added to verify its correctness. The output is
    now written out to a csv file. Numbers are in scientific notation (e.g.
    1.3e4). Convolution is implemented with three different methods, one in
    frequency domain, using the fast fourier transform algorithm and two in
    time domain, one which uses a parallel iterator to perform multiple
    operations simultaneously. The quickest function for convolution is the
    one that uses parallelism, while the FFT and regular basic versions are
    slower. FFT is not much faster than I expected, which might be because
    the wavelet is significantly shorter than the signal length. Convolution
    in frequency domain is comparatively quicker than in time domain when
    the two signals are of similar length.

--- rust/cwt-cli/src/main.rs
+++ rust/cwt-cli/src/main.rs
@@ -1,11 +1,27 @@
 use std::fs::File;
 use std::path::Path;
 
+use rustfft::FFTplanner;
+use rustfft::num_complex::Complex;
+use rustfft::num_traits::Zero;
+
+use itertools::Itertools;
+use rayon::prelude::*;
+
+mod iter;
+use iter::rangef;
+
+
+
+
 fn main() {
+    let input_file = Path::new("data.wav");
+    let output_file = Path::new("scaleogram.csv");
+
     println!("CWT implementation that can be put on a microcontroller");
 
     // Get signal y(t)
-    let mut inp_file = File::open(Path::new("./data.wav")).unwrap();
+    let mut inp_file = File::open(input_file).unwrap();
     let (header, data) = wav::read(&mut inp_file).unwrap();
     let fs = header.sampling_rate;
 
@@ -13,10 +29,11 @@ fn main() {
         wav::BitDepth::Sixteen(raw_signal) => {
             println!("read success (i16), length {:?}", raw_signal.len());
 
-            // Remap to range -1.0 to 1.0
+            // Remap to range -1.0 to 1.0, and take only 100ms
             let y = raw_signal
                 .iter()
                 .map(|x| (*x as f32) / (i16::MAX as f32))
+                .take((0.100 * fs as f32) as usize)
                 .collect::<Vec<f32>>();
 
             // Wavelet function
@@ -24,10 +41,20 @@ fn main() {
             let wvlt_bounds = [-3.0, 3.0];
 
             // Frequencies (1 to 9 kHz at interval of 10Hz)
-            let frequencies: Vec<f32> = IterStep::new(1000.0,9000.0,10.0).collect();
+            let frequencies: Vec<f32> = rangef(1000.0,9000.0,10.0).collect();
 
             // Do cwt
             let s = cwt(y, wvlt_fn, wvlt_bounds, frequencies, fs);
+
+            // Write cwt data to a file
+            let mut wtr = csv::Writer::from_path(output_file).unwrap();
+            for row in s.into_iter() {
+                let text_vec: Vec<String> = row.iter().map( |n| format!("{:e}",n) ).collect(); // Use sci notation
+                wtr.write_record(&text_vec).unwrap();
+            }
+            wtr.flush().unwrap();
+            
+
         }
         _ => panic!("read error or wrong wave type"),
     }
@@ -54,76 +81,121 @@ fn cwt(
     println!("cwt fn called");
 
     let step = 1.0/(fs as f32);
+    let mut s: Vec<Vec<f32>> = Vec::new();
 
     for f in &frequencies {
         let scale = 1.0/f;
-        let t = IterStep::new(wvlt_bounds[0]*scale, wvlt_bounds[1]*scale, step);
-        let wv: Vec<f32> = t.map(|t| wvlt_fn(t/scale)).rev().collect();
+        let t = rangef(wvlt_bounds[0]*scale, wvlt_bounds[1]*scale, step);
+        let k = 1.0 / scale.sqrt();
+        let wv: Vec<f32> = t.map( |t| k * wvlt_fn(t/scale) ).rev().collect();
 
-        // row = conv(y,fliplr(wv)) .* (1/sqrt(scale));
-        // s(i,:) = row(length(wv):end);
+        let row = conv_par(&y,&wv);
+        s.push(row);
     }
 
-    vec![vec![]] // Placeholder empty data
+    s
 }
 
+// do convolution the traditional way
+fn conv(sig: &Vec<f32>, fir: &Vec<f32>) -> Vec<f32> {
+    println!("conv called");
 
+    let mut y: Vec<f32> = vec![0.0;sig.len()];
 
-struct IterStep {
-    value: Option<f32>,
-    start: f32,
-    end: f32,
-    step: f32,
-}
-
-impl IterStep {
-    fn new(start: f32, end: f32, step: f32) -> IterStep {
-        IterStep { value: None, start, end, step }
-    }
-}
-
-impl Iterator for IterStep {
-    type Item = f32;
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.value {
-            Some(v) => { 
-                if v < self.end {
-                    self.value = Some(v + self.step);
-                    self.value
-                } else {
-                    None
-                }
-            },
-            None => {
-                self.value = Some(self.start);
-                self.value
-            },
+    for x in 0..sig.len() {
+        for i in 0..fir.len() {
+            if x+i >= sig.len() {
+                break;
+            } else {
+                y[x] += fir[i] * sig[x+i];
+            }
         }
     }
-}
-impl DoubleEndedIterator for IterStep {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        match self.value {
-            Some(v) => { 
-                if v > self.start {
-                    self.value = Some(v - self.step);
-                    self.value
-                } else {
-                    None
-                }
-            },
-            None => {
-                self.value = Some(self.end);
-                self.value
-            },
-        }
-    }
+
+    y
 }
 
-    // // Test iterator
-    // for i in IterStep::new(0.0,5.0,0.5) {
-    //     println!("i: {:?}",i);
+// do convolution using parallelism
+fn conv_par(sig: &Vec<f32>, fir: &Vec<f32>) -> Vec<f32> {
+    println!("conv called");
+
+    let mut y: Vec<f32> = vec![0.0;sig.len()];
+
+    // for x in 0..sig.len() {
+    //     for i in 0..fir.len() {
+    //         if x+i >= sig.len() {
+    //             break;
+    //         } else {
+    //             y[x] += fir[i] * sig[x+i];
+    //         }
+    //     }
     // }
-    // for i in IterStep::new(0.0,5.0,0.5).rev() {
-    //     println!("i: {:?}",i);
-    // }
+
+    // (0..sig.len()).into_par_iter().map(|ind| {
+    //     for i in 0..fir.len() {
+    //         if ind+i >= sig.len() {
+    //             break;
+    //         } else {
+    //             y[ind] += fir[i] * sig[ind+i];
+    //         }
+    //     }
+    // });
+
+    y.par_iter_mut().enumerate().for_each(|(ind, val)| {
+        for i in 0..fir.len() {
+            if ind+i >= sig.len() {
+                break;
+            } else {
+                *val = *val + fir[i] * sig[ind+i];
+            }
+        }
+    });
+
+    y
+}
+
+// do convolution quickly using FFT
+fn conv_fft(sig: &Vec<f32>, fir: &Vec<f32>) -> Vec<f32> {
+
+    println!("conv_fast called");
+
+    let n = sig.len() + fir.len() - 1;
+
+    // Time domain
+    let mut tsig: Vec<Complex<f32>> = sig.iter().pad_using(n, |_i| &0.0).map(|t| Complex::from(t)).collect();
+    let mut tfir: Vec<Complex<f32>> = fir.iter().pad_using(n, |_i| &0.0).map(|t| Complex::from(t)).collect();
+
+    // Frequency domain
+    let mut fsig: Vec<Complex<f32>> = vec![Complex::zero(); n];
+    let mut ffir: Vec<Complex<f32>> = vec![Complex::zero(); n];
+
+    println!("vecs prepared");
+
+    // Do FFT
+    let fft = FFTplanner::new(false).plan_fft(n);
+    fft.process(&mut tsig, &mut fsig);
+    fft.process(&mut tfir, &mut ffir);
+
+    println!("fft done");
+
+    // Elementwise multiplication
+    // Note that normally I would divide fsig and ffir by sqrt(n) but that isn't necessary here
+    let mut fres: Vec<Complex<f32>> = vec![Complex::zero(); n];
+    for i in 0..n {
+        fres[i] = fsig[i] * ffir[i];
+    }
+
+    println!("elementwise multiplication done");
+
+    // Do IFFT
+    let mut tres: Vec<Complex<f32>> = vec![Complex::zero(); n];
+    let fft = FFTplanner::new(true).plan_fft(n);
+    fft.process(&mut fres, &mut tres);
+
+    println!("ifft done");
+
+    // Make real and return
+    let result: Vec<f32> = tres.iter().map(|i| i.re).collect();
+    result
+
+}
