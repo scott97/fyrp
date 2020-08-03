ebb5f27432d85d8926296748ab9b1f089307322f
Scott97 <scottbarnard5@gmail.com>
2020-08-03 11:00:00 +1000

    Partially implement convolution using SIMD
    SIMD (single instruction multiple data) is a group of CPU instructions
    that perform an operation on multiple values simultaneously. I plan to
    use SIMD to speed up convolution, which is very slow. I started with the
    faster library, but I had difficulties, so I switched to the packed simd
    library. This feature is still a work in progress.

--- rust/cwt-cli/src/wavelets.rs
+++ rust/cwt-cli/src/wavelets.rs
@@ -2,8 +2,12 @@ use rustfft::num_complex::Complex;
 use rustfft::num_traits::Zero;
 use rustfft::FFTplanner;
 
+use faster::*;
 use itertools::Itertools;
+use packed_simd::*;
 use rayon::prelude::*;
+
+
 
 use super::iter::rangef;
 
@@ -50,7 +54,6 @@ pub fn cwt_par(
     fs: u32,
 ) -> Vec<Vec<f32>> {
     let step = 1.0 / (fs as f32);
-    
     frequencies
         .par_iter()
         .map(|f| {
@@ -59,7 +62,7 @@ pub fn cwt_par(
             let k = 1.0 / scale.sqrt();
             let wv: Vec<f32> = t.map(|t| k * wvlt_fn(t / scale)).rev().collect();
 
-            conv(&y, &wv)[wv.len()..].to_vec()
+            conv_simd2(&y, &wv)[wv.len()..].to_vec()
         })
         .collect()
 }
@@ -82,10 +85,154 @@ fn conv(x: &Vec<f32>, h: &Vec<f32>) -> Vec<f32> {
     y
 }
 
+// do convolution using SIMD - note this lib is complicated, lets not use it
+fn conv_simd(x: &Vec<f32>, h: &Vec<f32>) -> Vec<f32> {
+    // dbg!("conv_simd");
+
+    let lx = x.len();
+    let lh = h.len();
+    let lxh = lx + lh - 1;
+
+    let mut x = x.to_vec(); // doing the copying probably is slowing it down
+    x.extend(vec![0.0f32; lh - 1].into_iter());
+    let mut y = vec![0.0; lxh];
+
+    #[cfg(not(any(
+        target_feature = "avx2",
+        target_feature = "avx",
+        target_feature = "sse",
+        target_feature = "avx512"
+    )))]
+    {
+        dbg!("SIMD not supported?");
+    }
+
+    // doesn't crash, but does it work and is it faster?
+    // also i think this might be the wrong length
+    for m in 0..lh {
+        y = (
+            (&y[0..lx]).simd_iter(f32s(0.0)),
+            (&x[m..(m + lx)]).simd_iter(f32s(0.0)),
+        )
+            .zip()
+            .simd_map(|(y_p, x_s)| y_p + x_s * f32s(h[m]))
+            .scalar_collect();
+    }
+
+    y
+}
+
+// do convolution using SIMD (alternative library)
+// fn conv_simd2(x: &Vec<f32>, h: &Vec<f32>) -> Vec<f32> {
+//     // dbg!("conv_simd");
+
+//     let lx = x.len();
+//     let lxch = lx - (lx % 16); // Length of x rounded down to nearest 16.
+//     let lh = h.len();
+//     let lxh = lx + lh - 1;
+
+//     let mut y = vec![0.0; lxh]; // It might be worth trying extending to the next 16, and padding with zeros
+
+//     for m in 0..lh {
+//         // TODO - handle left and right zeros
+//         // TODO - shift x
+
+//         dbg!(h[m]);
+
+//         // Process chunks
+//         dbg!("process chunks");
+//         for ch in (0..lxch).step_by(16) {
+//             let x_chunk = f32x16::from_slice_unaligned(&x[ch..]);
+//             let y_chunk = f32x16::from_slice_unaligned(&y[ch..]);
+//             let result = y_chunk + x_chunk * f32x16::splat(h[m]);
+//             result.write_to_slice_unaligned(&mut y[ch..]);
+//         }
+//         // Remaining elements
+//         dbg!("single elements");
+//         for i in (lxch..lx) {
+//             y[i] += x[i] * h[m];
+//         }
+//         dbg!(&y);
+//     }
+
+//     y
+// }
+
+// /// works on chunks, pads to get up to a chunk size
+// /// issue: shifts x in wrong direction
+// fn conv_simd2(x: &Vec<f32>, h: &Vec<f32>) -> Vec<f32> {
+//     dbg!("conv_simd");
+
+//     let lx = x.len();
+//     let lh = h.len();
+//     let lxh = lx + lh - 1;
+//     let lxch = lx - (lx % 16) + 16; 
+//     let lxhch = lxch + lh;
+
+//     let mut y = vec![0.0; lxhch];
+//     let mut xm = x.to_vec();
+//     xm.resize(lxhch, 0.);
+    
+//     dbg!(&xm); // initial values of extended x
+
+//     for m in 0..lh {
+//         // TODO - fix bugs
+//         dbg!(h[m]);
+//         for ch in (0..lxch).step_by(16) {
+//             dbg!(&ch);
+//             let x_chunk = f32x16::from_slice_unaligned(&xm[(ch+m)..(ch+m+16)]); // Issue, shifts wrong direction. This isn't as simple as changing the sign, I also need to consider the edge of the backing array.
+//             let y_chunk = f32x16::from_slice_unaligned(&y[(ch)..(ch+16)]);
+//             let result = y_chunk + x_chunk * f32x16::splat(h[m]);
+//             result.write_to_slice_unaligned(&mut y[ch..]);
+//         }
+//         dbg!(&y);
+//     }
+
+//     //y[0..lxh].to_vec()
+//     y.truncate(lxh);
+//     y
+// }
+
+
+/// correct implementation
+fn conv_simd2(x: &Vec<f32>, h: &Vec<f32>) -> Vec<f32> {
+    dbg!("conv_simd");
+
+    let lx = x.len();
+    let lh = h.len();
+    let lxh = lx + lh - 1;
+    let lxch = lx - (lx % 16) + 16; 
+    let lxhch = lxch + lh;
+
+    let mut y = vec![0.0; lxhch];
+
+    let mut xm = vec![0.; lh];
+    xm.extend(x.iter()); // pad left w/ zeros so shifts of x don't read outside array.
+    xm.resize(lxhch, 0.); // pad right w/ zeros to chunk size.
+    
+    dbg!(&xm); // initial values of extended x
+
+    for m in 0..lh {
+        dbg!(h[m]);
+        for ch in (0..lxch).step_by(16) {
+            dbg!(&ch);
+            let x_chunk = f32x16::from_slice_unaligned(&xm[(ch+lh-m)..(ch+lh-m+16)]);
+            let y_chunk = f32x16::from_slice_unaligned(&y[(ch)..(ch+16)]);
+            let result = y_chunk + x_chunk * f32x16::splat(h[m]);
+            result.write_to_slice_unaligned(&mut y[ch..]);
+        }
+        dbg!(&y);
+    }
+
+    //y[0..lxh].to_vec()
+    y.truncate(lxh);
+    y
+}
+
+
 // do convolution using FFT
 fn conv_fft(sig: &Vec<f32>, fir: &Vec<f32>) -> Vec<f32> {
     //dbg!("conv_fft");
-    
     let n = sig.len() + fir.len() - 1;
 
     // Time domain
@@ -110,10 +257,11 @@ fn conv_fft(sig: &Vec<f32>, fir: &Vec<f32>) -> Vec<f32> {
     fft.process(&mut tfir, &mut ffir);
 
     // Elementwise multiplication
-    // Note that normally I would divide fsig and ffir by sqrt(n) but that isn't necessary here
+    // Dividing each individually by sqrt(n) is the same as dividing both by n.
     let mut fres: Vec<Complex<f32>> = vec![Complex::zero(); n];
+    let n_inv = 1. / (n as f32);
     for i in 0..n {
-        fres[i] = fsig[i] * ffir[i];
+        fres[i] = fsig[i] * ffir[i] * n_inv;
     }
 
     // Do IFFT
@@ -126,7 +274,7 @@ fn conv_fft(sig: &Vec<f32>, fir: &Vec<f32>) -> Vec<f32> {
     result
 }
 
-// do convolution using parallelism
+// do convolution using parallelism !!!not correct
 // fn conv_par(sig: &Vec<f32>, fir: &Vec<f32>) -> Vec<f32> {
 //     let mut y: Vec<f32> = vec![0.0; fir.len() + sig.len() - 1];
 
