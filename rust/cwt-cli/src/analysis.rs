b10f3a16b17a7b485bbb0c401c64d1a1f6fe0de7
Scott97 <scottbarnard5@gmail.com>
2020-08-05 12:00:00 +1000

    Remove exec_time macro which caused compile errors

--- rust/cwt-cli/src/analysis.rs
+++ rust/cwt-cli/src/analysis.rs
@@ -1,9 +1,7 @@
-#[exec_time]
 pub fn find_max(s: &Vec<Vec<f32>>) -> Vec<Vec<f32>> {
     todo!();
 }
 
-#[exec_time]
 pub fn threshold(s: &mut Vec<Vec<f32>>, min: f32) {
     for row in s.into_iter() {
         for val in row {
