dd01d3a8b7a2cf37abbe78860b71b7875d100cd5
Scott97 <scottbarnard5@gmail.com>
2020-08-18 15:42:42 +1000

    Implement an iterator that takes from a circular buffer
    In the future, there will be a need to have a circular buffer between
    the data acquisition code and the cwt analysis code. To reduce data
    copying, I plan on passing a lazy iterator into the process/process_par
    method rather than a vector copy of the data. This commit implements an
    iterator for a circular buffer. In a segmented cwt, there is some
    overlap between reading of chunks of data. The overlap is the length of
    the wavelet minus one, at the largest scale (lowest frequency band). The
    circular buffer needs to advance by the length of the chunk minus the
    overlap. Therefore the iterator lets you specify the amount of elements
    to take and the amound of elements to peek. Unit tests for this iterator
    are passed.

--- rust/cwt-cli/src/iter.rs
+++ rust/cwt-cli/src/iter.rs
@@ -1,3 +1,5 @@
+use queues::*;
+
 pub fn rangef(start: f32, end: f32, step: f32) -> Rangef {
     Rangef {
         value: None,
@@ -53,10 +55,66 @@ impl DoubleEndedIterator for Rangef {
     }
 }
 
+pub struct CircIter<'a> {
+    pub cbuf: &'a mut CircularBuffer<f32>,
+    pub take: usize,
+    pub peek: usize,
+}
+
+impl<'a> Iterator for CircIter<'a> {
+    type Item = f32;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.take > 0 {
+            self.take -= 1;
+            Some(self.cbuf.remove().unwrap())
+        } else if self.peek > 0 {
+            self.peek -= 1;
+            Some(self.cbuf.peek().unwrap())
+        } else {
+            None
+        }
+    }
+}
+
 // Unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_circ_iter() {
+        let mut cbuf = CircularBuffer::<f32>::new(5);
+        cbuf.add(1.);
+        cbuf.add(2.);
+        cbuf.add(3.);
+        cbuf.add(4.);
+        cbuf.add(5.);
+
+        {
+            let mut iter = CircIter {
+                cbuf: &mut cbuf,
+                take: 3,
+                peek: 1,
+            };
+
+            assert_eq!(Some(1.), iter.next());
+            assert_eq!(Some(2.), iter.next());
+            assert_eq!(Some(3.), iter.next());
+            assert_eq!(Some(4.), iter.next());
+            assert_eq!(None, iter.next());
+        }
+        {
+            let mut iter = CircIter {
+                cbuf: &mut cbuf,
+                take: 2,
+                peek: 0,
+            };
+
+            assert_eq!(Some(4.), iter.next());
+            assert_eq!(Some(5.), iter.next());
+            assert_eq!(None, iter.next());
+        }
+    }
 
     #[test]
     fn test_forward() {
