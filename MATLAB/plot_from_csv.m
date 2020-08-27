3ec56e956a8ace3967b525479b6da68065f2c5e6
Scott97 <scottbarnard5@gmail.com>
2020-08-27 17:17:48 +1000

    Add choice of csv file to plot as a scaleogram
    The segmented CWT produces multiple scaleogram csv files. This changes
    the MATLAB m file so that I can choose which to plot.

--- MATLAB/plot_from_csv.m
+++ MATLAB/plot_from_csv.m
@@ -1,10 +1,13 @@
-% Import cwt data
-s = csvread('scaleogram.csv');
+function plot_from_csv(idx)
+    % Import cwt data
+    file = sprintf('scaleogram%d.csv', idx)
+    s = csvread(file);
 
-% Plot CWT
-fs = 44100;
+    % Plot CWT
+    fs = 44100;
 
-[h,l] = size(s)
-t = (0:l-1)/fs;
-f = linspace(1000,9000,h);
-plot_utils.scaleogram(s,f,t,[0 100],[1 9],'my cwt (rust impl)');
+    [h,l] = size(s)
+    t = (0:l-1)/fs;
+    f = linspace(1000,9000,h);
+    plot_utils.scaleogram(s,f,t,[0 100],[1 9],'my cwt (rust impl)');
+end
