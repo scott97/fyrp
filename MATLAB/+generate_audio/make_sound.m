a1764d71bf28925fd96c210f86a481a163f61baf
Scott97 <scottbarnard5@gmail.com>
2020-10-10 19:07:22 +1100

    When synthesizing bubble audio, set zeta from radius
    This sets zeta = 0.02 for 1 mm bubbles and zeta = 0.013 for 5 mm bubbles
    and interpolates for those in between.

--- MATLAB/+generate_audio/make_sound.m
+++ MATLAB/+generate_audio/make_sound.m
@@ -1,32 +1,49 @@
 % Solving 2ODE's is slow so this just uses an algebraic expression.
 function d = make_sound(R_0, ddot_0, T, t)
 
+    % Bubbles oscillate according to this differential equation:
+    % eq1: R_0 * δdotdot + b * δdot + (3 * K_p * P_0) / (ρ_0 * R_0) * δ = 0
+    % 
+    % A commonly used formula for oscillations is:
+    % δdotdot + (2*ζ*ωn)*δdot + (ωn^2)*δ = 0
+    % 
+    % This has a solution:
+    % eq2: δ = ωn / sqrt(1-ζ^2) * e^(-ζ*ωn*t) * sin(ωd*t)
+    % where ωd = ωn * sqrt(1-ζ^2)
+    % 
+    % Divide eq1 by R_0.
+    % δdotdot + (b/R_0)*δdot + (3 * K_p * P_0) / (ρ_0 * R_0^2) * δ 
+    % 
+    % Therefore:
+    % ωn^2 = (3 * K_p * P_0) / (ρ_0 * R_0^2)
+    % ωn = sqrt( (3 * K_p * P_0) / (ρ_0 * R_0^2) )
+
     % Constants
     K_p = 1.4; % 1.4
     p_0 = 1000; % 1000 kg/m^3
     P_0 = 101325; % 1 atm or 101,325 Pa
-    b = 0.6; % (need to research typical values)
 
-    % Calculated Constants
-    a = R_0;
-    b;
-    c = (3 * K_p * P_0) / (p_0 * R_0);
+    % Calculate zeta from radius.
+    % b is not known, but ζ is.
+    % Known points (we interpolate for in between)
+    % ζ = 0.02 for 1 mm and ζ = 0.013 for 5 mm
+    z_p = [0.02, 0.013];
+    r_p = [1e-3, 5e-3];
 
-    % More Calculated Constants
-    k = b/(2*a);
-    w = sqrt( (c/a) - (k^2) );
-    M = ddot_0/w;
+    z = z_p(1) + ( R_0 - r_p(1) ) * ( z_p(2) - z_p(1) ) / ( r_p(2) - r_p(1) );
 
+    % Calculating ωn and ωd.
+    wn = sqrt( (3 * K_p * P_0) / (p_0 * R_0^2) );
+    wd = wn * sqrt(1-z^2);
 
     % Evaluate
-    d = M * exp(-k .* (t-T)) .* sin(w .* (t-T)) .* ((t-T) > 0);
+    d = wn / sqrt(1 - z^2) .* exp(-z .* wn .* (t-T)) .* sin(wd .* (t-T)) .* ((t-T) > 0);
 
     % The exp function is causing infinities or NaNs for large negative numbers of (t-T)
     % These matrix elements should be zero because they occur before T.
     % This patches them up as zero.
     TF = isnan(d);
     d(TF) = 0;
-
 
 end
 
